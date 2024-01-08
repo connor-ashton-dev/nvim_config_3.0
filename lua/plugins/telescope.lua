8a026ef80a652e80faed4d25b75d816132fd99b9
Connor Ashton <cjconnorashton@gmail.com>
2024-01-08 13:32:58 -0700

    telescope changes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,60 +10,60 @@ return {
 			},
 		},
 		config = function()
+			require("telescope").setup({
+				defaults = {
+					-- prompt_prefix = " ",
+					-- selection_caret = ">",
+					path_display = { "smart" },
+				},
+
+				pickers = {
+					find_files = {
+						find_command = {
+							"rg",
+							"--files",
+							"-g",
+							"!.git",
+							"-g",
+							"!node_modules/*",
+							"-g",
+							"!target/*",
+						},
+						-- theme = "ivy",
+						previewer = false,
+						-- no_ignore = true,
+						hidden = true,
+						prompt_title = "All Files",
+					},
+
+					git_files = {
+						theme = "dropdown",
+						previewer = false,
+						prompt_title = "Git Files",
+					},
+					spell_suggest = {
+						theme = "cursor",
+						-- previewer = false,
+						prompt_title = "Spelling Suggestions",
+					},
+
+					treesitter = {
+						theme = "ivy",
+						-- previewer = true,
+						prompt_title = "Variables",
+					},
+					extensions = {
+						fzf = {
+							fuzzy = true, -- false will only do exact matching
+							override_generic_sorter = true, -- override the generic sorter
+							override_file_sorter = true, -- override the file sorter
+							case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+							-- the default case_mode is "smart_case"
+						},
+					},
+				},
+			})
 			require("telescope").load_extension("fzf")
 		end,
-		opts = {
-			defaults = {
-				-- prompt_prefix = " ",
-				-- selection_caret = ">",
-				path_display = { "smart" },
-			},
-
-			pickers = {
-				find_files = {
-					find_command = {
-						"rg",
-						"--files",
-						"-g",
-						"!.git",
-						"-g",
-						"!node_modules/*",
-						"-g",
-						"!target/*",
-					},
-					-- theme = "dropdown",
-					-- previewer = false,
-					-- no_ignore = true,
-					hidden = true,
-					prompt_title = "All Files",
-				},
-
-				git_files = {
-					theme = "dropdown",
-					previewer = false,
-					prompt_title = "Git Files",
-				},
-				spell_suggest = {
-					theme = "cursor",
-					-- previewer = false,
-					prompt_title = "Spelling Suggestions",
-				},
-
-				treesitter = {
-					theme = "ivy",
-					-- previewer = true,
-					prompt_title = "Variables",
-				},
-				extensions = {
-					fzf = {
-						fuzzy = true, -- false will only do exact matching
-						override_generic_sorter = true, -- override the generic sorter
-						override_file_sorter = true, -- override the file sorter
-						case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-						-- the default case_mode is "smart_case"
-					},
-				},
-			},
-		},
 	},
 }
