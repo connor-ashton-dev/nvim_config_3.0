e5313dae1d01bfaf24d313bdd787e79b5b4fea8b
Connor Ashton <cjconnorashton@gmail.com>
2025-08-04 21:50:49 -0600

    cleaned stuff up and added supermaven

--- lua/plugins/go.lua
+++
@@ -1,6 +0,0 @@
-return {
-	-- "fatih/vim-go",
-	-- event = { "BufReadPre", "BufNewFile" },
-	-- config = function() -- vim.cmd("set go:go_auto_type_info=1")
-	-- end,
-}
--- lua/plugins/treesitter-context.lua
+++
@@ -1,5 +0,0 @@
-return {
-	-- "nvim-treesitter/nvim-treesitter-context",
-	-- event = { "BufReadPre", "BufNewFile" },
-	-- opts = { mode = "cursor", max_lines = 3 },
-}
