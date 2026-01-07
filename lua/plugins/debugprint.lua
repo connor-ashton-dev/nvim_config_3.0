dbb24577eaf8931b202ddec147d1d9069d173f56
Connor Ashton <cjconnorashton@gmail.com>
2026-01-07 14:43:09 -0700

    tressitter

--- lua/plugins/debugprint.lua
+++ lua/plugins/debugprint.lua
@@ -1,6 +1,15 @@
 return {
 	"andrewferrier/debugprint.nvim",
 
+	dependencies = {
+		"ibhagwan/fzf-lua", -- Optional: If you want to use the `:Debugprint search` command with fzf-lua
+		"nvim-telescope/telescope.nvim", -- Optional: If you want to use the `:Debugprint search` command with telescope.nvim
+		"folke/snacks.nvim", -- Optional: If you want to use the `:Debugprint search` command with snacks.nvim
+	},
+
+	lazy = false, -- Required to make line highlighting work before debugprint is first used
+	version = "*", -- Remove if you DON'T want to use the stable version
+
 	opts = {
 		print_tag = "DEBUG 🔍",
 		display_counter = false,
@@ -32,17 +41,4 @@ return {
 		},
 		-- … Other options
 	},
-
-	dependencies = {
-		"nvim-mini/mini.nvim", -- Optional: Needed for line highlighting (full mini.nvim plugin)
-		-- ... or ...
-		"nvim-mini/mini.hipatterns", -- Optional: Needed for line highlighting ('fine-grained' hipatterns plugin)
-
-		"ibhagwan/fzf-lua", -- Optional: If you want to use the `:Debugprint search` command with fzf-lua
-		"nvim-telescope/telescope.nvim", -- Optional: If you want to use the `:Debugprint search` command with telescope.nvim
-		"folke/snacks.nvim", -- Optional: If you want to use the `:Debugprint search` command with snacks.nvim
-	},
-
-	lazy = false, -- Required to make line highlighting work before debugprint is first used
-	version = "*", -- Remove if you DON'T want to use the stable version
 }
